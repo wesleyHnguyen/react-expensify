03b31764042b6609eb135c0fcff38335a347a3f3
alison <hongthanhnguyen85@gmail.com>
2020-03-20 15:11:18 +0700

    Add expense summary to dash board

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import getVisibleExpenses from '../selectors/expenses';
+import total from '../selectors/expenses-total'
+import  numeral from 'numeral';
 
 const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
        {props.expenses.map((expense)=>(
            <ExpenseListItem key={expense.id} {...expense}/>
-       ))}
-        
+       ))}        
     </div>
 );
 
